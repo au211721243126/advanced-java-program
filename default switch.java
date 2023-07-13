0065137098e7b4e492c0390346eec512ce0ff05a
Ponnarasan RK <117897532+au211721243126@users.noreply.github.com>
2023-07-13 09:41:56 +0530

    Create default switch.java

---
+++ default switch.java
@@ -0,0 +1,12 @@
+int day = 5;
+
+switch(day) {
+  case 1:
+    System.out.println("Monday");
+  case 2:
+    System.out.println("Tuesday");
+  case 3:
+    System.out.println("Wednesday");
+  default: 
+    System.out.println("Another day");
+}
